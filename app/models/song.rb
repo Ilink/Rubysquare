58f74e9f213d2f859ab25affcf77c92b4ae2e771
Ian Link <ian@linkportfolio.net>
2011-10-02 20:29:04 -0700

    added basic json binding functionality

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,4 @@
 class Song < ActiveRecord::Base
   validates_presence_of :title, :location
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,9 @@ class User < ActiveRecord::Base
   def self.authenticate(email, password)
     user = find_by_email(email)
     if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
-      user #returns
+      user
     else
-      nil #returns
+      nil
     end
   end
 
