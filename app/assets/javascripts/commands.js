abb73759c8632bc79608e7900d62050b2453ce25
Ian Link <ian@linkportfolio.net>
2011-10-21 19:33:46 -0700

    removed some old code

--- app/assets/javascripts/commands.js
+++ app/assets/javascripts/commands.js
@@ -104,10 +104,6 @@ rubysquare.commands.test_command_child = function(arg){
 }
 rubysquare.commands.test_command_child.prototype = rubysquare.commands.test_command.prototype;  //set the prototype: access to methods, properties, etc
 
-
-//Do i want arguments so it knows what command to affect?
-//right now the onus is on the user to keep track of the affected command names
-
 //TODO: make me change only the setting
 rubysquare.commands.shuffle_command = (function(command_pointer){
     var command_pointer = command_pointer;
@@ -115,22 +111,10 @@ rubysquare.commands.shuffle_command = (function(command_pointer){
        execute : function(){
            //TODO: change shuffle button visuals
            rubysquare.settings['shuffle'] = true //will be an AJAX call to update the database
-//           rubysquare.log(rubysquare.settings['shuffle']);
-//
-//           //eventually this solution won't be good enough - there could be many things calling "next" with a bind. I need a way of storing EVERY instance of a node getting bound to a command
-//           //this could be within the JSON for bindings, as long as only one selector is used
-//           //that could get messy too, with more complex selectors
-//           $(rubysquare.ui.node_names['next_button']).unbind('click', command_pointer.execute); //bindings are closures - have to remove the old one before changing the command
-//           command_pointer = rubysquare.commands.next_song_shuffle_command;
-//           $(rubysquare.ui.node_names['next_button']).bind('click', command_pointer.execute);
        },
        unexecute : function(){
            //TODO: revert changes to shuffle button visuals
            rubysquare.settings['shuffle'] = false //AJAX call later...
-//           rubysquare.log(rubysquare.settings['shuffle']);
-//           $(rubysquare.ui.node_names['next_button']).unbind('click', command_pointer.execute);  //bindings are closures - have to remove the old one before changing the command
-//           command_pointer = rubysquare.commands.next_song_straight_command;
-//           $(rubysquare.ui.node_names['next_button']).bind('click', command_pointer);
        }
    }
 })();
