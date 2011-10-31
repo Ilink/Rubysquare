74a6f4b87037d82d08c4c1c5005c78a69a2769aa
Ian Link <ian@linkportfolio.net>
2011-10-30 17:20:29 -0700

    moved loader functionality out of this and into helper.js

--- app/assets/javascripts/ui.js
+++ app/assets/javascripts/ui.js
@@ -1,15 +1,5 @@
 rubysquare.ui_manager = function(){
     if (this instanceof rubysquare.ui_manager){
-        //this function may seem specific but its a very general case that encompasses 99% of music-playing behavior
-        this.play_from_available = function( song_index, available_playlist, now_playing_playlist ){
-            now_playing_playlist.copy_from( available_playlist.playlist );
-            console.log("attempting to play song at location: " + now_playing_playlist.playlist[song_index].location);
-            rubysquare.music.current_index = song_index;
-            rubysquare.music.set_song( now_playing_playlist.playlist[song_index].location );
-            rubysquare.music.play();
-            console.log(now_playing_playlist.playlist);
-            // TODO: should update the DB "now playing" playlist at this point
-        }
     }
     else return new rubysquare.ui_manager();
 }
