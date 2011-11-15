1e8fe00b44b6c01d35d9b04dfd7d7e3b96952e16
Ian Link <ian@linkportfolio.net>
2011-11-14 17:05:29 -0800

    fixing migration error, wip

--- db/migrate/20111115004157_adjust_playlist_foreign_key.rb
+++ db/migrate/20111115004157_adjust_playlist_foreign_key.rb
@@ -1,10 +1,12 @@
 class AdjustPlaylistForeignKey < ActiveRecord::Migration
-  #change_table(:playlists) do |t|
-  #  t.column :user_id, :integer
-  #end
-
   def change
-    change_column :playlists, :user_id, :integer
+    change_table(:playlists) do |t|
+      t.change :user_id, :integer
+    end
   end
+
+  #def change
+  #  change_column :playlists, :user_id, :integer, :force => true
+  #end
 
 end
