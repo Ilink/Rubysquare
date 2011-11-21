8ad5cacc6a7c8cd8ea326d8f3008f5084cdc057c
Ian Link <ian@linkportfolio.net>
2011-11-21 11:47:04 -0800

    css things

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,51 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 song_locations = [
-  'http://www.podtrac.com/pts/redirect.mp3/downloads.newyorker.com/mp3/outloud/111107_outloud_mendelsohn.mp3',
-  'http://www.podtrac.com/pts/redirect.mp3/downloads.newyorker.com/mp3/outloud/111024_outloud_batuman.mp3',
-  'http://www.podtrac.com/pts/redirect.mp3/downloads.newyorker.com/mp3/campaign/111103_politicalscene.mp3',
-  'http://www.podtrac.com/pts/redirect.mp3/downloads.newyorker.com/mp3/campaign/111027_politicalscene.mp3',
-  'http://www.podtrac.com/pts/redirect.mp3/downloads.newyorker.com/mp3/fiction/111018_fiction_mccann.mp3',
-  'http://www.podtrac.com/pts/redirect.mp3/downloads.newyorker.com/mp3/fiction/110819_fiction_rushdie.mp3'
-]
-
-song_locations2 = [
-    {
-      "title" => ''
-    }
+  {
+    "title" => 'Daniel Mendelsohn on the Illiad',
+    "artist" => 'The New Yorker',
+    "album" => 'The New Yorker Outloud',
+    "location" => 'http://www.podtrac.com/pts/redirect.mp3/downloads.newyorker.com/mp3/outloud/111107_outloud_mendelsohn.mp3'
+  },
+  {
+    "title" => 'Elif Batuman on Biodiversity',
+    "artist" => 'The New Yorker',
+    "album" => 'The New Yorker Outloud',
+    "location" => 'http://www.podtrac.com/pts/redirect.mp3/downloads.newyorker.com/mp3/outloud/111024_outloud_batuman.mp3'
+  },
+  {
+      "title" => 'Occupy Wallstreet',
+      "artist" => 'The New Yorker',
+      "album" => 'The Political Scene',
+      "location" => 'http://www.podtrac.com/pts/redirect.mp3/downloads.newyorker.com/mp3/campaign/111103_politicalscene.mp3'
+  },
+  {
+      "title" => 'Discussion of the Republican Field',
+      "artist" => 'The New Yorker',
+      "album" => 'The Political Scene',
+      "location" => 'http://www.podtrac.com/pts/redirect.mp3/downloads.newyorker.com/mp3/campaign/111027_politicalscene.mp3'
+  },
+  {
+      "title" => 'Colum McCann reads Benedict Kiely',
+      "artist" => 'The New Yorker',
+      "album" => 'The New Yorker Fiction',
+      "location" => 'http://www.podtrac.com/pts/redirect.mp3/downloads.newyorker.com/mp3/fiction/111018_fiction_mccann.mp3'
+  },
+  {
+      "title" => 'Salman Rushdie reads Donald Barthelme',
+      "artist" => 'The New Yorker',
+      "album" => 'The New Yorker Fiction',
+      "location" => 'http://www.podtrac.com/pts/redirect.mp3/downloads.newyorker.com/mp3/fiction/110819_fiction_rushdie.mp3'
+  }
 ]
 
 Song.delete_all
 for i in 1..100
+  rand_song = song_locations[rand(6)] # rand goes from 0 to arg-1, so this has a range of 0 to 5
   Song.create(
-    :title => "Podcast #{i}",
-    :artist => "The New Yorker #{i}",
-    :album => "",
-    :location => song_locations[rand(6)] # rand goes from 0 to arg-1, so this has a range of 0 to 5
+    :title => rand_song['title'],
+    :artist => rand_song['artist'],
+    :album => rand_song['album'],
+    :location => rand_song['location']
   )
 end
